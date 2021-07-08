ee7d6779007fdfd183d81f857a7d48670a30c5c4
kishan <kishankarena619@gmail.com>
2021-07-08 15:52:50 +0530

    Scaffold the app

---
+++ Desktop/Intershala/chat-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import 'rsuite/dist/styles/rsuite-default.css';
+import './styles/main.scss'
+
+
+function App() {
+  return (
+    <div>
+      hello
+
+     
+    </div>
+  );
+}
+
+export default App;
