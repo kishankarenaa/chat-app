eb184bb61bdbd35bd32fffbc4dbf93db788e4cb1
kishan <kishankarena619@gmail.com>
2021-07-08 17:29:36 +0530

    initialized firebase app

---
+++ Desktop/Intershala/chat-app/src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyDPfugIiGuvX6GXc0qLpliZ8GkSiMFQvJo",
+  authDomain: "chat-web-app-cebb4.firebaseapp.com",
+  projectId: "chat-web-app-cebb4",
+  storageBucket: "chat-web-app-cebb4.appspot.com",
+  messagingSenderId: "829553964571",
+  appId: "1:829553964571:web:6361ee668eea484a28eabe"
+};
+
+ const app =firebase.initializeApp(config);
