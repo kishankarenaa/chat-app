822be59ec23d14e596e0f8780967369800379483
kishan <kishankarena619@gmail.com>
2021-07-10 16:39:51 +0530

    added sign in page and sign in with facebook/google

--- Desktop/Intershala/chat-app/src/misc/firebase.js
+++ Desktop/Intershala/chat-app/src/misc/firebase.js
@@ -1,12 +1,16 @@
-import firebase from "firebase/app";
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
-  apiKey: "AIzaSyDPfugIiGuvX6GXc0qLpliZ8GkSiMFQvJo",
-  authDomain: "chat-web-app-cebb4.firebaseapp.com",
-  projectId: "chat-web-app-cebb4",
-  storageBucket: "chat-web-app-cebb4.appspot.com",
-  messagingSenderId: "829553964571",
-  appId: "1:829553964571:web:6361ee668eea484a28eabe"
+  apiKey: 'AIzaSyDPfugIiGuvX6GXc0qLpliZ8GkSiMFQvJo',
+  authDomain: 'chat-web-app-cebb4.firebaseapp.com',
+  projectId: 'chat-web-app-cebb4',
+  storageBucket: 'chat-web-app-cebb4.appspot.com',
+  messagingSenderId: '829553964571',
+  appId: '1:829553964571:web:6361ee668eea484a28eabe',
 };
 
- const app =firebase.initializeApp(config);
+const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
