5ffcec4139751e68716c219626c9d0c84c5f5dff
kishan <kishankarena619@gmail.com>
2021-07-10 11:17:53 +0530

    Created public and private Route

--- Desktop/Intershala/chat-app/src/App.js
+++ Desktop/Intershala/chat-app/src/App.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React from 'react';
+import { Switch } from 'react-router';
 
 import 'rsuite/dist/styles/rsuite-default.css';
-import './styles/main.scss'
-
+import SignIn from './pages/SignIn';
+import './styles/main.scss';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
   return (
-    <div>
-      hello
-
-     
-    </div>
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
   );
 }
 
